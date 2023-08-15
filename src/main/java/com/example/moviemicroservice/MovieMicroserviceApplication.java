220f3f1fa93a5afaaab69b763ca5fd27b42fdcbb
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-08-15 22:33:46 +0200

    initial

--- src/main/java/com/example/moviemicroservice/MovieMicroserviceApplication.java
+++ src/main/java/com/example/moviemicroservice/MovieMicroserviceApplication.java
@@ -2,7 +2,8 @@ package com.example.moviemicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MovieMicroserviceApplication {
 
